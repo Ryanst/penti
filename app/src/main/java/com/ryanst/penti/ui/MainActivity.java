b3b278afdfc146362f290e547405d73a2080f1c8
Ryanst <zhengjuntong@gmail.com>
2016-08-03 18:59:56 +0800

    databinding OK

--- app/src/main/java/com/ryanst/penti/ui/MainActivity.java
+++ app/src/main/java/com/ryanst/penti/ui/MainActivity.java
@@ -1,10 +1,9 @@
 package com.ryanst.penti.ui;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -14,44 +13,40 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.ryanst.penti.R;
 import com.ryanst.penti.constant.PentiConst;
 import com.ryanst.penti.core.BaseActivity;
+import com.ryanst.penti.databinding.ActivityMainBinding;
 
 import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.drawer_layout)
-    DrawerLayout drawer;
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
-    @BindView(R.id.nav_view)
-    NavigationView navView;
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private NavigationView navView;
     private FragmentManager fragmentManager;
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initView();
+        fragmentManager = getSupportFragmentManager();
+        showFragment(PentiConst.FRAGMENT_TYPE_PENTI_WANG);
+    }
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void initView() {
+
+        toolbar = binding.layoutContentMain.layoutToolBar.toolbar;
+        drawer = binding.drawerLayout;
+        navView = binding.navView;
+
+        setSupportActionBar(toolbar);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.pentiwang, R.string.pentiwang);
@@ -59,9 +54,6 @@ public class MainActivity extends BaseActivity
         toggle.syncState();
 
         navView.setNavigationItemSelectedListener(this);
-
-        fragmentManager = getSupportFragmentManager();
-        showFragment(PentiConst.FRAGMENT_TYPE_PENTI_WANG);
     }
 
     private void showFragment(String type) {
