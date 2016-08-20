44486c61e81de9c0a70fa5ada4c0fe973363ed5d
Ryanst <zhengjuntong@gmail.com>
2016-08-20 14:38:09 +0800

    add Stetho and StrictMode

--- app/src/main/java/com/ryanst/penti/core/MyApplication.java
+++ app/src/main/java/com/ryanst/penti/core/MyApplication.java
@@ -3,10 +3,15 @@ package com.ryanst.penti.core;
 import android.app.Application;
 import android.content.Context;
 
+import com.facebook.stetho.Stetho;
 import com.github.moduth.blockcanary.BlockCanary;
+import com.orhanobut.logger.LogLevel;
 import com.ryanst.penti.BuildConfig;
+import com.ryanst.penti.util.StrictModeUtil;
 import com.ryanst.penti.widget.AppBlockCanaryContext;
 import com.ryanst.penti.widget.UCEHandler;
+
+import java.util.logging.Logger;
 
 import butterknife.ButterKnife;
 
@@ -19,9 +24,21 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         application = this;
+
+        StrictModeUtil.init();
+
         ButterKnife.setDebug(BuildConfig.DEBUG);
+
         BlockCanary.install(this, new AppBlockCanaryContext()).start();
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                        .build());
+
 //        initUncaughtException();
     }
 
---
+++ app/src/main/java/com/ryanst/penti/util/StrictModeUtil.java
@@ -0,0 +1,32 @@
+package com.ryanst.penti.util;
+
+import android.os.Build;
+import android.os.StrictMode;
+
+import com.ryanst.penti.BuildConfig;
+
+/**
+ * 开启严格模式，检测内存、硬盘等敏感操作，线程监控出现问题会出对话框提示
+ * Created by zhaokaiqiang on 15/11/9.
+ */
+public class StrictModeUtil {
+
+    public static void init() {
+        if (false && BuildConfig.DEBUG && Build.VERSION.SDK_INT > Build.VERSION_CODES.FROYO) {
+
+            //线程监控，会弹框哦
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDialog()
+                    .build());
+
+            //VM监控
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
+    }
+
+}
