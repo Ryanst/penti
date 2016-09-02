a0fc2f82c406144bc685ecc57fcb97c781dea519
Ryanst <zhengjuntong@gmail.com>
2016-09-02 10:41:27 +0800

    update image

--- app/src/main/java/com/ryanst/penti/ui/NewsListFragment.java
+++ app/src/main/java/com/ryanst/penti/ui/NewsListFragment.java
@@ -32,10 +32,8 @@ import com.ryanst.penti.widget.recyclerview.RecyclerViewStateUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
